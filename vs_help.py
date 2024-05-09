31c0413724c2b24c00c31bf0feb279f92df09e16
artyfoxx <98079477+artyfoxx@users.noreply.github.com>
2024-05-09 17:39:30 +0600

    Add files via upload

--- vs_help.py
+++ vs_help.py
@@ -25,6 +25,7 @@ from inspect import signature
 # fine_dehalo2
 # insane_aa
 # upscaler
+# custom_mask
 
 # Lanczos-based resize by "*.mp4 guy", ported from AviSynth version with minor additions.
 # It is well suited for downsampling. Cropping parameters added in the form of **kwargs.
@@ -822,7 +823,7 @@ def tp7_deband_mask(clip: VideoNode, thr: float | list[float] = 8, scale: float 
         expr = 'x y max 0.5 +'
     
     if fake_prewitt:
-        clip = custom_mask(clip, scale = scale)
+        clip = custom_mask(clip, 1, scale)
     else:
         clip = core.std.Prewitt(clip, scale = scale)
     
@@ -978,7 +979,7 @@ def fine_dehalo(clip: VideoNode, rx: float = 2, ry: float | None = None, thmi: i
         dehaloed = fine_dehalo_contrasharp(dehaloed, clip, contra)
     
     if fake_prewitt:
-        edges = custom_mask(clip)
+        edges = custom_mask(clip, 1)
     else:
         edges = core.std.Prewitt(clip)
     
@@ -1265,21 +1266,23 @@ def custom_mask(clip: VideoNode, mask: int = 0, scale: float = 1.0, boost: bool 
         expr3 = f'x 2 * 0.86 {offset} + 0.0 max 1.0 min'
     
     if mask == 0:
+        pass
+    elif mask == 1:
         clip = core.std.Expr([core.std.Convolution(clip, [1, 1, 0, 1, 0, -1, 0, -1, -1], divisor = 1, saturate = False),
                               core.std.Convolution(clip, [1, 1, 1, 0, 0, 0, -1, -1, -1], divisor = 1, saturate = False),
                               core.std.Convolution(clip, [1, 0, -1, 1, 0, -1, 1, 0, -1], divisor = 1, saturate = False),
                               core.std.Convolution(clip, [0, -1, -1, 1, 0, -1, 1, 1, 0], divisor = 1, saturate = False)],
                               expr1)
-    elif mask == 1:
+    elif mask == 2:
         clip = core.std.Expr([core.std.Convolution(clip, [5, 10, 5, 0, 0, 0, -5, -10, -5], divisor = 4, saturate = False),
                               core.std.Convolution(clip, [5, 0, -5, 10, 0, -10, 5, 0, -5], divisor = 4, saturate = False)],
                               expr2)
-    elif mask == 2:
+    elif mask == 3:
         clip = core.std.Expr([core.std.Convolution(clip, [8, 16, 8, 0, 0, 0, -8, -16, -8], divisor = 8, saturate = False),
                               core.std.Convolution(clip, [8, 0, -8, 16, 0, -16, 8, 0, -8], divisor = 8, saturate = False)],
                               expr2)
     else:
-        raise ValueError(f'{func_name}: Please use 0...2 mask value')
+        raise ValueError(f'{func_name}: Please use 0...3 mask value')
     
     if boost:
         clip = core.std.Expr(clip, expr3)
